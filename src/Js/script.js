844e679f9861a09dc58498d7277c1a44ca278fdd
Pap <125478255+PapLion@users.noreply.github.com>
2024-02-27 20:08:59 -0500

    Portafolio-Dani (Beta)

--- src/Js/script.js
+++
@@ -1,7 +0,0 @@
-let tog = document.getElementById('tog');
-let lbl_tog = document.getElementById('lbl_tog');
-
-tog.addEventListener('change', (event) => {
-    let isChecked = event.target.checked;
-    document.body.classList.toggle('dark-theme', isChecked);
-});
