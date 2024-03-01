25d1b47b2c8503f64c58e1f119996dce6571f276
ExperienceV <132963842+ExperienceV@users.noreply.github.com>
2024-03-01 14:25:16 -0600

    Diseño de URLS para cada seccion e implementacion de scrip de contacto.
    Co-authored-by: Dani <PapLion@users.noreply.github.com>

--- convert.py
+++ convert.py
@@ -1,19 +1,42 @@
 import json
 import uuid
-import json
+import requests
+
 
 def contacts(dict: dict):
-    id=uuid.uuid4()
-    nameJson = f"entity{id}.json"
-    nameTxt = f"entity{id}.txt"
+    id = uuid.uuid4()
+    nameJson = f"Contacts/entity{id}.json"
+    nameTxt = f"Contacts/entity{id}.txt"
     with open(nameJson, 'w') as file:
         json.dump(dict, file, indent=4)
 
-    
-    with open(nameJson, 'r') as file:
+    with open(nameJson, 'r', encoding='utf-8') as file:
         datos = json.load(file)
 
-    with open(nameTxt, 'w') as file:
+    with open(nameTxt, 'w', encoding='utf-8') as file:
         for key, value in datos.items():
-            file.write(f"{key}: {value}\n")   
-    print(f"Convertion succefully!")
+            file.write(f"{key}: {value}\n")
+
+    print(f"Conversión exitosa!")
+
+    # URL del webhook de Discord
+    webhook_url = 'https://discord.com/api/webhooks/1212950229427556382/dyJvsn6IdAs5aEUL8zEB1OTAZOh8bf7zLEMKAgVAb4K_4hwqhBAEG8arLL7IhqdonM83'
+
+    # Abrir y leer el contenido del archivo .txt
+    with open(nameJson, 'r') as file:
+        file_content = file.read()
+
+    # Construir el payload para el webhook
+    payload = {
+        'file': ("pandaContact.txt", file_content, 'text/plain')
+    }
+
+    # Enviar la solicitud POST al webhook de Discord
+    response = requests.post(webhook_url, files=payload)
+
+    # Verificar si la solicitud se realizó con éxito
+    if response.status_code == 204:
+        print("El archivo .txt se envió correctamente al canal de Discord.")
+    else:
+        print("Ocurrió un error al enviar el archivo .txt al canal de Discord.")
+
--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 from typing import Annotated
 from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from convert import contacts
 
 app = FastAPI()
-templates = Jinja2Templates(directory="src")
-
-@app.get("/", response_class=HTMLResponse)
+templates = Jinja2Templates(directory="src/templates")
+app.mount("/static", StaticFiles(directory="src/static"), name="Static")
+#pandi panda usa el chat de Live Share csm
+@app.get("/Home", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/contact", response_class=HTMLResponse)
+
+@app.get("/Home/Contact", response_class=HTMLResponse)
 async def contact(request: Request):
     return templates.TemplateResponse("contact.html", {"request": request})
 
-@app.post("/contact/form")
+
+@app.post("/Home/Contact/Form")
 async def contact(request:Request, name: Annotated[str, Form()], email: Annotated[str, Form()], message: Annotated[str, Form()]):
     data = {"name":name,"email":email,"message":message}
     contacts(data)
     return templates.TemplateResponse("response.html", {"request": request})
+
+@app.get("/Home#About")
+async def contact(request:Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/Home#Skills")
+async def contact(request:Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/Home#Tecnologies")
+async def contact(request:Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/Home#Proyects")
+async def contact(request:Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+
 
